d73990c95aca3d63c7986a533d27271d30f102bc
Julien DAUPHANT <judauphant@gmail.com>
2015-02-18 19:06:47 +0100

    Correct " to ' in js and camelCase style

--- www/app.js
+++ www/app.js
@@ -1,30 +1,30 @@
-var API_ENDPOINT = "http://localhost:9000";
-var BROWSER = detect_browser();
-var STORY = "4-year-old-s-dream-of-being-ups-driver-comes-true";
+var API_ENDPOINT = 'http://localhost:9000';
+var BROWSER = detectBrowser();
+var STORY = '4-year-old-s-dream-of-being-ups-driver-comes-true';
 
 
-
-function detect_browser(){
+function detectBrowser()
+{
 	if(Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0)
 	{
 		if( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false )
 		{
-			return "safari_ios";
+			return 'safari_ios';
 		}
-		return "safari";
+		return 'safari';
 	}
 	if(typeof InstallTrigger !== 'undefined')
-		return "firefox";
+		return 'firefox';
 	if(!!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0)
-		return "opera";
+		return 'opera';
 	if(!!window.chrome)
-		return "chrome";
+		return 'chrome';
 	if(/*@cc_on!@*/false || !!document.documentMode)
-		return "ie";	
-	return "unknown"
+		return 'ie';	
+	return 'unknown'
 }
 
-function retrieve_token()
+function retrieveToken()
 {
 	var request = new XMLHttpRequest();
 	request.open('POST', API_ENDPOINT+'/tokens');
@@ -32,16 +32,16 @@ function retrieve_token()
 	request.onload = function() {
 	    if (request.status === 201) {
 	        var token = JSON.parse(request.responseText).tokens;
-	        retrieve_story(token,STORY)
+	        retrieveStory(token,STORY)
 	    }
 	    else {
-	    	alert("Unexpected answer")
+	    	alert('Unexpected answer')
 	    }
 	};
-	request.send("{}");
+	request.send('{}');
 }
 
-function retrieve_story(token,slug)
+function retrieveStory(token,slug)
 {
 	var request = new XMLHttpRequest();
 	request.open('GET', API_ENDPOINT+'/stories?slug='+slug);
@@ -50,36 +50,36 @@ function retrieve_story(token,slug)
 	request.onload = function() {
 	    if (request.status === 200) {
 	        var story = JSON.parse(request.responseText).stories;
-	        show_story(story);
+	        showStory(story);
 	    }
 	    else {
-	    	alert("Unexpected answer")
+	    	alert('Unexpected answer')
 	    }
 	};
 	request.send();
 }
 
-function box_to_dom(box)
+function boxToDom(box)
 {
 	if(['chrome','ie','safari','opera'].indexOf(BROWSER) > -1)
 	{
-		return box_to_dom_video(box);
+		return boxToDomVideo(box);
 	}
-	return box_to_dom_img(box);
+	return boxToDomImg(box);
 }
 
-function video_fallback(video) {
+function videoFallback(video) {
     var div = document.createElement('div');
     div.innerHTML = video.innerHTML;
     video.parentNode.replaceChild(div, video);
 }
 
-function box_to_dom_video(box)
+function boxToDomVideo(box)
 {
 	var video = document.createElement('video');
 	video.width = box.width;
 	video.height = box.height;
-	video.class = "box";
+	video.class = 'box';
 	video.loop = true;
 	video.controls = false;
 	video.autoplay = true;
@@ -93,7 +93,7 @@ function box_to_dom_video(box)
 		{
 			var source = document.createElement('source');
 			source.src=format.href;
-			source.type="video/"+format.type;
+			source.type='video/'+format.type;
 			video.appendChild(source);
 		}
 		else if('gif' === format.type)
@@ -108,10 +108,10 @@ function box_to_dom_video(box)
 	return video;
 }
 
-function box_to_dom_img(box)
+function boxToDomImg(box)
 {
 	var image = document.createElement('img');
-	image.class = "box";
+	image.class = 'box';
 	box.formats.forEach(function(format) {
 		if('gif' === format.type)
 		{
@@ -124,59 +124,59 @@ function box_to_dom_img(box)
 }
 
 
-function add_video_fallback()
+function addVideoFallback()
 {
 	var videos = document.querySelectorAll('video');
 	for (var i=0; i<videos.length; i++){
 		var video = videos[i],
 		    sources = video.querySelectorAll('source'),
 		        lastsource = sources[sources.length-1];
-		lastsource.addEventListener('error', video_fallback(video), false);
+		lastsource.addEventListener('error', videoFallback(video), false);
 	}
 }
 
-function story_to_dom(story)
+function storyToDom(story)
 {
 	var ret = document.createElement('div');
 	story.boxes.forEach(function(box) {
-		ret.appendChild(box_to_dom(box));
+		ret.appendChild(boxToDom(box));
 		ret.appendChild(document.createElement('br'));
 		ret.appendChild(document.createElement('br'));		
 	});
 	return ret;
 }
 
-function play_all_video()
+function playAllVideo()
 {
-	var videos = document.getElementsByTagName("video")
+	var videos = document.getElementsByTagName('video')
 	for (var i=0; i<videos.length; i++){
 	  videos[i].play();
 	}
 }
 
-function youtube_to_dom(source)
+function youtubeToDom(source)
 {
-	var iframe = document.createElement("iframe");
-	iframe.width="520";
-	iframe.height="315";
-	iframe.src="https://www.youtube.com/embed/"+source.id;
-	iframe.frameborder="0";
+	var iframe = document.createElement('iframe');
+	iframe.width='520';
+	iframe.height='315';
+	iframe.src='https://www.youtube.com/embed/'+source.id;
+	iframe.frameborder='0';
 	iframe.allowfullscreen=true;
 	return iframe;
 }
 
-function show_story(story)
+function showStory(story)
 {	
-	document.title = "SIZ - "+story.title;
-	document.getElementById("story_title").innerHTML = story.title;
-	var storyNode = document.getElementById("story_boxes");
-	storyNode.parentNode.replaceChild(story_to_dom(story),storyNode);
-	if(story.source.type === "youtube")
+	document.title = 'SIZ - '+story.title;
+	document.getElementById('story_title').innerHTML = story.title;
+	var storyNode = document.getElementById('story_boxes');
+	storyNode.parentNode.replaceChild(storyToDom(story),storyNode);
+	if(story.source.type === 'youtube')
 	{
-		document.getElementById("story_video").appendChild(youtube_to_dom(story.source));
+		document.getElementById('story_video').appendChild(youtubeToDom(story.source));
 	}
-	//add_video_fallback();
-	play_all_video();
+	//addVideoFallback();
+	playAllVideo();
 }
 
-retrieve_token()
+retrieveToken()
