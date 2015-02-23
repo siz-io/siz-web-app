f5d4fd54baa6ab19324f4ca09edb7153c29282e7
axlhgl <axel.hauguel@viewrz.com>
2015-02-23 18:57:09 +0100

    Added overlay for iPad

--- www/js/overlay.js
+++ www/js/overlay.js
@@ -1,6 +1,6 @@
 function overlay() {
     
-    if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+    if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))|| (navigator.userAgent.match(/iPad/i))) {
         document.getElementById("overlay").style.display="inline";
         document.getElementById("backtoapp").onclick = function(){
             document.getElementById("overlay").style.display="none";
