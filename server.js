468f4591a95595b2186660dd14fe073673d12718
guillaumervls <guillaumervls@server.fake>
2015-03-31 23:27:38 +0200

    added card view

--- server.js
+++ server.js
@@ -24,6 +24,23 @@ app.get('/', function (req, res) {
   res.render('app');
 });
 
+// Card
+app.get('/card/:slug', function (req, res) {
+  request(constants.API_ENDPOINT + '/stories?slug=' + req.params.slug, function (err, apiRes, body) {
+    try {
+      var story = body.stories;
+      if (!story) throw new Error();
+      story.shareUrl = req.protocol + '://' + req.headers.host + req.url;
+      story.encodedShareUrl = encodeURIComponent(story.shareUrl);
+      story.JSON = JSON.stringify(story).replace(/\//g, '\\/');
+      res.render('card', story);
+    } catch (err) {
+      res.statusCode = apiRes.statusCode;
+      res.render('error');
+    }
+  });
+});
+
 // Stories
 app.get('/stories/:slug', function (req, res) {
   request(constants.API_ENDPOINT + '/stories?slug=' + req.params.slug, function (err, apiRes, body) {
