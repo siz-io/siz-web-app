9ece068b0739836497372233e022deb6ca1f4648
guillaumervls <guillaumervls@server.fake>
2015-04-30 14:58:58 +0200

    hide powered-by on non-embed

--- static/src/js/embed/main.js
+++ static/src/js/embed/main.js
@@ -1,3 +1,9 @@
+var displayPoweredBy = true;
+try {
+  displayPoweredBy = window.top.location.host !== window.location.host || window.top === window;
+} catch (ignored) {}
+if (displayPoweredBy) document.querySelector('.powered-by').style.display = 'block';
+
 var story = window._storyData;
 story.aspectRatio = story.boxes[0].width / story.boxes[0].height;
 story.nBoxes = Math.min(story.boxes.length, 4); // Fix old stories
