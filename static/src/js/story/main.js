3decc94ad959d623309603fb0e3c9b317dc3e1ed
guillaumervls <guillaumervls@server.fake>
2015-06-23 15:50:13 +0200

    Fix #16

--- static/src/js/story/main.js
+++ static/src/js/story/main.js
@@ -5,7 +5,6 @@ if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i
   document.getElementById('overlay').style.display = 'inline';
   document.getElementById('backtoapp').onclick = function () {
     document.getElementById('overlay').style.display = 'none';
-    window.location.href = '#story_video';
   };
 }
 
