7d1563c0fdbede5d9374944edabbf5e8f477cf69
guillaumervls <guillaumervls@users.noreply.github.com>
2015-09-17 16:20:06 +0200

    Remove popover

--- static/src/js/story/main.js
+++ static/src/js/story/main.js
@@ -1,14 +1,14 @@
-require('../lib/app-link');
+// require('../lib/app-link');
 
 // iOS popover
-if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
-  document.getElementById('overlay').style.display = 'inline';
-  document.getElementById('backtoapp').onclick = function () {
-    document.getElementById('overlay').style.display = 'none';
-  };
-}
+// if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+//   document.getElementById('overlay').style.display = 'inline';
+//   document.getElementById('backtoapp').onclick = function () {
+//     document.getElementById('overlay').style.display = 'none';
+//   };
+// }
 
 // Android alert
-var ANDROID_CTA = 'We have an awesome app designed for mobile. It looks a trillion times better!!! Do you want to download it?';
-if (navigator.userAgent.match(/Android/i) && window.confirm(ANDROID_CTA)) // eslint-disable-line no-alert
-  window.location.href = '/get-the-app?origin=pop-up';
+// var ANDROID_CTA = 'We have an awesome app designed for mobile. It looks a trillion times better!!! Do you want to download it?';
+// if (navigator.userAgent.match(/Android/i) && window.confirm(ANDROID_CTA)) // eslint-disable-line no-alert
+//   window.location.href = '/get-the-app?origin=pop-up';
